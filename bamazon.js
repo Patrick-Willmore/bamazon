6fac9db558eafd4ed29b2a1aaa9e3f4b39bd9614
Patrick Willmore <patman1127@gmail.com>
2018-02-10 15:37:15 -0700

    pseudocode

---
+++ bamazon.js
@@ -0,0 +1,11 @@
+//install packages
+//require
+//inquirer.prompt .then(answers)
+    //   * The first should ask them the ID of the product they would like to buy.
+   //* The second message should ask how many units of the product they would like to buy.
+//function check quantity
+    //if >= 
+        // func buy item
+            //update quantity in mySQL database
+
+    //total cost??
