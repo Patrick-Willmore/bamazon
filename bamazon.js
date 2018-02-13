6edf158fa2ca9f38efd2638b5fa82e950723b357
Patrick Willmore <patman1127@gmail.com>
2018-02-13 11:53:11 -0700

    table

--- bamazon.js
+++ bamazon.js
@@ -1,4 +1,74 @@
 //install packages
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var {table} = require("table");
+
+
+var sqlConnection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "bitemsdb"
+  });
+
+  sqlConnection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    printItemList();
+    console.log("connected as id " + sqlConnection.threadId);
+  });
+
+  const printItemList = function() {
+    return new Promise( (resolve, reject) => {
+        sqlConnection.query("SELECT * FROM `bitemsdb`.`products` LIMIT 1000;", function (err, res) {
+            if (err) reject(Error(err));
+            // Log all results of the SELECT statement
+            var data = [];
+            data.push(
+                [
+                    ('id'),
+                    ('product_name'),
+                    ('dept_name'),
+                    ('price'),
+                    ('quantity')
+                ]
+            );
+            res.forEach(element => {
+                data.push(
+                    [
+                        element.item_id,
+                        element.product_name,
+                        element.depart_name,
+                        element.price,
+                        element.quantity
+                    ]
+                );
+            });
+            console.log(table(data));
+            resolve();
+        });
+    })
+    start();
+}
+  function start() {
+    inquirer
+      .prompt({
+        name: "userPrompt",
+        type:"input",
+        message: "what would you like to buy (#)?"
+        
+      })
+      .then(function(answer) {
+        console.log(answer);
+
+      });
+  }
+
 //require
 //inquirer.prompt .then(answers)
     //   * The first should ask them the ID of the product they would like to buy.
